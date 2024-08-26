c4fd0760e6d62f7a2e031054fe5eca9764029b57
Tim <81175852+wharvex@users.noreply.github.com>
2024-08-26 19:38:51 -0400

    first commit

---
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AvaloniaApplication1.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private int _count;
+
+    [ObservableProperty]
+    private string? _username;
+
+    [ObservableProperty]
+    private string? _password;
+
+    [ObservableProperty]
+    private string _greeting = "hey";
+
+    partial void OnGreetingChanged(string value)
+    {
+        _count++;
+    }
+
+    [RelayCommand]
+    private async Task Login()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(3));
+        Greeting = "yay";
+    }
+
+    [RelayCommand]
+    private void SetGreeting(string name)
+    {
+        Greeting += $" {name}";
+    }
+}
